f73638a08102c1a5c6974e7a386eaf255228f6ad
hyegyeong-kang <rkdgprud96@gmail.com>
2023-02-27 11:55:17 +0900

    add practice files

---
+++ springbasic/src/main/java/kosa/di/Factory.java
@@ -0,0 +1,18 @@
+package kosa.di;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration		 /* 설정파일 역할 */
+public class Factory {
+	
+	@Bean
+	public Dao oracleDao(){
+		return new OracleDao();
+	}
+
+	@Bean
+	public Service writeService2() {
+		return new WriteService();
+	}
+}
