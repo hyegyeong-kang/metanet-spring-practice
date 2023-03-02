9ed0e4533b895f3c3ca4ec36e645441b1331d26c
hyegyeong-kang <rkdgprud96@gmail.com>
2023-03-02 12:33:28 +0900

    add Board test files

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -47,4 +47,29 @@ public class BoardMapperTests {
 		List<BoardVO> list = mapper.getList();
 		list.forEach(board -> log.info(board));
 	}
+	
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		log.info("DELETE COUNT: " + mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int cnt = mapper.update(board);
+		log.info("UPDATE COUNT: " + cnt);
+	}
 }
